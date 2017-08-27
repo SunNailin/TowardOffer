59fb757dd99dc20366459673a84045920e5385d3
sunnailin <sdsunnailin@163.com>
2017-08-27 22:50:42 +0800

    add gitignore

--- TowardOffer/HasSubtree/HasSubtree.cpp
+++ TowardOffer/HasSubtree/HasSubtree.cpp
@@ -19,7 +19,7 @@ public:
 	//1.首先判断B的根结点是不是A的某个结点
 	//2.如果B的根结点是A的某个结点，那么判断B的左右子结点是否是A的该结点的左右子结点
 	//3.如果是，则继续判断B的子结点的子结点是否A的子结点的子结点
-	//4.递归调用
+	//4.递归调用对
 	bool HasSubtree(TreeNode* pRoot1, TreeNode* pRoot2)
 	{
 		if (pRoot1 == NULL && pRoot2 == NULL)
